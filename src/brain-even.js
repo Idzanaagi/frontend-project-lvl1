4cd3917139065b347433a756c61ce186452aed4a
Idzanaagi <anmorgunowa@gmail.com>
2021-11-11 19:11:57 +0300

    add EvenGame to src

---
+++ src/brain-even.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+
+function gameEven() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const getRandomNumber = () => Math.floor(Math.random() * 100);
+
+  for (let i = 1; i <= 3; i++) {
+    const number = getRandomNumber();
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('You answer: ');
+    if (number % 2 === 0 && userAnswer === 'yes' || number % 2 !== 0 && userAnswer === 'no') {
+      console.log('Correct!');
+    }
+    if (number % 2 === 0 && userAnswer === 'no') {
+      return (`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
+    }
+    if (number % 2 !== 0 && userAnswer === 'yes') {
+      return (`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+    }
+  }
+  console.log('Congratulation!');
+}
+console.log(gameEven());
