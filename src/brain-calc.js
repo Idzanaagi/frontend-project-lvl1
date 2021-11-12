b32f4c306920d3b531531339f6ac1637d8d8b5aa
Idzanaagi <anmorgunowa@gmail.com>
2021-11-12 23:22:39 +0300

    add working version brain-calc (NO MODULES) - update Makefile, bin/ & src/brain-calc.js

---
+++ src/brain-calc.js
@@ -0,0 +1,45 @@
+import readlineSync from 'readline-sync';
+
+function calc() {
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 100);
+  }
+  let calculationResult;
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('What is the result of the expression?');
+
+  for (let i = 1; i <= 3; i += 1) {
+    const firstRandomNumber = getRandomNumber();
+    const secondRandomNumber = getRandomNumber();
+
+    const arrOperator = ['+', '-', '*'];
+    const random = Math.floor(Math.random() * arrOperator.length);
+    const randomOperator = (arrOperator[random]);
+
+    console.log(`Question: ${firstRandomNumber} ${randomOperator} ${secondRandomNumber}`);
+
+    const userAnswer = readlineSync.question('You answer: ');
+
+    if (randomOperator === '+') {
+      calculationResult = firstRandomNumber + secondRandomNumber;
+    }
+    if (randomOperator === '-') {
+      calculationResult = firstRandomNumber - secondRandomNumber;
+    }
+    if (randomOperator === '*') {
+      calculationResult = firstRandomNumber * secondRandomNumber;
+    }
+
+    if (Number(userAnswer) !== calculationResult) {
+      return (`${userAnswer} is wrong answer ;(. Correct answer was ${calculationResult}.\nLet's try again, ${userName}`);
+    }
+    if (Number(userAnswer) === calculationResult) {
+      console.log('Correct!');
+    }
+  }
+  return (`Congratulation, ${userName}!`);
+}
+
+export default calc;
